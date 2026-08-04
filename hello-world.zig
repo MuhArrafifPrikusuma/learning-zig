b3892bdf1d78017593374e7907a98af39cf46d7c
MuhArrafifPrikusuma <muhammadarrafif1308@gmail.com>
2026-08-04 17:34:51 +0800

    some more basics

--- hello-world.zig
+++ hello-world.zig
@@ -74,3 +74,78 @@ test "while with continue" {
     }
     try std.testing.expect(sum == 4);
 }
+
+test "while with break" {
+    var sum: u8 = 0;
+    var i: u8 = 1;
+    while (i <= 10) : (i += 1) {
+        if (i == 5) break;
+        sum += i;
+    }
+    try std.testing.expect(sum == 10);
+}
+
+test "for loops" {
+    const string = [_]u8{ 'h', 'e', 'l', 'l', 'o' };
+
+    for (string, 0..) |character, index| {
+        std.debug.print("character: {c} at index: {d}\n", .{ character, index });
+    }
+    for (string) |character| {
+        _ = character;
+    }
+
+    // we don't take character here we only take the index
+    for (string, 0..) |_, index| {
+        std.debug.print("index: {d}\n", .{index});
+    }
+}
+
+test "test fibonacci functions" {
+    const val = fibonacci(10);
+    try std.testing.expect(@TypeOf(val) == u32);
+    try std.testing.expect(val == 55);
+}
+test "faster fibonacci" {
+    const val = fasterfib(10);
+    try std.testing.expect(val == 55);
+}
+
+test "defer" {
+    var x: i16 = 5;
+    // arbitrary block scoping to shrink scope inside function
+    // oh and also defer here is block scoped unlike go function scope
+    {
+        defer x += 2;
+        std.debug.print("x: {d}\n", .{x});
+        try std.testing.expect(x == 5);
+    }
+    std.debug.print("x: {d}\n", .{x});
+    try std.testing.expect(x == 7);
+}
+
+// this will execute the defer on reverse order just like in go
+test "multi defer" {
+    var x: f32 = 5;
+    {
+        defer x += 2; // <- then this
+        defer x /= 2; // <- this first
+    }
+    try std.testing.expect(x == 4.5);
+}
+
+// functions
+// recursion might cause stack overflow
+fn fibonacci(n: u32) u32 {
+    if (n == 0 or n == 1) return n;
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+fn fasterfib(n: u32) u32 {
+    var sum: u32 = 0;
+    var i: u32 = 1;
+    while (i <= n) : (i += 1) {
+        sum += i;
+    }
+    return sum;
+}
